eb313b55ef652095916ea962bc00a762869a51b4
mobarqaw <mobarqaw@microsoft.com>
2023-12-14 13:18:38 +0300

    first commit

---
+++ HttpEventListener.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.Tracing;
+using System.Text;
+namespace HttpsTrace
+{
+    internal sealed class HttpEventListener : EventListener
+    {
+        protected override void OnEventSourceCreated(EventSource eventSource)
+        {
+            // Allow internal HTTP logging
+            if (eventSource.Name == "Private.InternalDiagnostics.System.Net.Http")
+            {
+                EnableEvents(eventSource, EventLevel.LogAlways);
+            }
+        }
+
+        protected override void OnEventWritten(EventWrittenEventArgs eventData)
+        {
+            // Log whatever other properties you want, this is just an example
+            var sb = new StringBuilder().Append($"{eventData.TimeStamp:HH:mm:ss.fffffff}[{eventData.EventName}] ");
+            if (eventData.EventName == "EventCounters")
+                return;
+            for (int i = 0; i < eventData.Payload?.Count; i++)
+            {
+              
+                if (i > 0)
+                    sb.Append(", ");
+                sb.Append(eventData.PayloadNames?[i]).Append(": ").Append(eventData.Payload[i]);
+            }
+            try
+            {
+                Console.WriteLine(sb.ToString());
+            }
+            catch { }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+
+namespace HttpsTrace
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); 
+          //  var listener = new HttpEventListener(_output);
+
+
+            using var listener2 = new HttpEventListener();
+
+            using var client = new HttpClient();
+
+            while (true)
+            {
+                try
+                {
+                    // Ask the user to enter a URL
+                    Console.Write("Please enter a URL: ");
+                    var url = Console.ReadLine();
+
+                    // Example: Send a GET request to a URL
+                    var response = await client.GetAsync(url);
+                    Console.WriteLine("Response received:");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"An error occurred: {e.Message}");
+                }
+
+                // Ask the user if they want to exit
+                Console.Write("Do you want to exit? (y/n): ");
+                var exit = Console.ReadLine();
+                if (exit.ToLower() == "y")
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    // Keep the listener around while you want the logging to continue, dispose it after.
+
+}
