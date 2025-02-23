dc8f10e7229641b3ec76487e5f63cc450fdade9b
mobarqaw <mobarqaw@microsoft.com>
2025-02-23 11:52:13 +0300

    check X509ChainStatus adn get the value in table

--- SslClientCertificates.cs
+++ SslClientCertificates.cs
@@ -21,8 +21,9 @@ namespace HttpsTrace
     public class SslClientCertificates
     {
         static Options2? Options;
-        static string htmlFilePath = "SslClientCertificates_output.html";
+        static string htmlFilePath = "HttpsTraceLog.html";
         static StreamWriter htmlWriter;
+        static int h2 = 0;
 
         public static void Main(string[] args)
         {
@@ -39,17 +40,17 @@ namespace HttpsTrace
                     if (!string.IsNullOrEmpty(o.CertificatePath))
                     {
 
-                 
+
                         WriteSection("Client Certificate Path", $"<p>{o.CertificatePath}</p><p><textarea rows=\"15\" class=\"block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500\">{EncodePfxToBase64(o.CertificatePath)}</textarea></p>");
 
                         X509Certificate2 clientCert = new X509Certificate2(o.CertificatePath, o.CertificatePassword);
 
-                      
+
                         WriteSection("Selected Client Certificate", $"<p>Subject:{clientCert.Subject}</p><p>Issuer:{clientCert.Issuer}</p>");
 
                     }
 
-                   
+
                 })
                 .WithNotParsed(errors =>
                 {
@@ -60,9 +61,21 @@ namespace HttpsTrace
                     return;
                 });
 
-           
-                string hostname = Options.ServerName;
-                int port = 443;
+
+                string hostname;
+                int port;
+
+                if (Options.ServerName.Contains(":"))
+                {
+                    var parts = Options.ServerName.Split(':');
+                    hostname = parts[0];
+                    port = int.Parse(parts[1]);
+                }
+                else
+                {
+                    hostname = Options.ServerName;
+                    port = 443;
+                }
                 WriteSection("Environment Variables",
                     $"<p>WEBSITE_LOAD_USER_PROFILE: {Environment.GetEnvironmentVariable("WEBSITE_LOAD_USER_PROFILE")}</p>" +
                     $"<p>WEBSITE_LOAD_ROOT_CERTIFICATES: {Environment.GetEnvironmentVariable("WEBSITE_LOAD_ROOT_CERTIFICATES")}</p>");
@@ -73,7 +86,7 @@ namespace HttpsTrace
                     {
                         using (SslStream sslStream = new SslStream(client.GetStream(), false, ValidateServerCertificate, SelectLocalCertificate))
                         {
-                           
+
                             sslStream.AuthenticateAsClient(hostname);
 
                             X509Certificate serverCertificate = sslStream.RemoteCertificate;
@@ -88,9 +101,9 @@ namespace HttpsTrace
                                 WriteSection("Server Certificate", "<p class='text-red-500'>No server certificate received.</p>");
                             }
                             WriteSection("Client Certificate Authentication",
-                               sslStream.IsMutuallyAuthenticated ? "<p class='text-green-500'>Client certificate was required and successfully authenticated.</p>" :
+                               sslStream.IsMutuallyAuthenticated ? "<p class='text-green-500'>Client certificate was required </p>" :
                                "<p>---------</p>");
-                           
+
                         }
                     }
                 }
@@ -109,7 +122,7 @@ namespace HttpsTrace
         private static void WriteHtmlHeader()
         {
             htmlWriter.WriteLine("<html><head><title>HTTPS Trace Report</title>");
-         
+
             htmlWriter.WriteLine("<script src=\"https://cdn.tailwindcss.com\"></script>");
             htmlWriter.WriteLine("</head><body class='p-8 bg-gray-900 text-white'>");
             htmlWriter.WriteLine("<h1 class='text-3xl font-bold text-center mb-6 text-gray-200'>HTTPS Trace Report</h1>");
@@ -118,7 +131,7 @@ namespace HttpsTrace
         private static void WriteSection(string title, string content)
         {
             htmlWriter.WriteLine($"<section class='bg-gray-800 shadow-lg rounded-lg p-6 mb-6'>");
-            htmlWriter.WriteLine($"<h2 class='text-xl font-semibold mb-3 text-gray-300'>{title}</h2>");
+            htmlWriter.WriteLine($"<h2 class='text-xl font-semibold mb-3 text-gray-300'> {h2++} -- {title}</h2>");
             htmlWriter.WriteLine(content);
             htmlWriter.WriteLine("</section>");
         }
@@ -130,15 +143,32 @@ namespace HttpsTrace
 
         private static bool ValidateServerCertificate(
             object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+
+
+
         {
-            WriteSection("Server Certificate Validation", $"<p>Server Certificate: {certificate?.Subject} tumpprint {certificate?.GetCertHashString()} </p>");
+            string chainStatusTable = string.Empty;
+            if (chain != null)
+            {
+                chainStatusTable = "<table class='table-auto w-full'><thead><tr><th class='px-4 py-2'>Status</th><th class='px-4 py-2'>Status Information</th></tr></thead><tbody>";
+                foreach (X509ChainStatus chainStatus in chain.ChainStatus)
+                {
+                    chainStatusTable += $"<tr><td class='border px-4 py-2'>{chainStatus.Status}</td><td class='border px-4 py-2'>{chainStatus.StatusInformation}</td></tr>";
+                }
+                chainStatusTable += "</tbody></table>";
+
+            }
+
+            WriteSection("Server Certificate Validation", $"<ol>Server Certificate: {certificate?.Subject} <li>Thumbprint {certificate?.GetCertHashString()}</li><li> SSL Policy Errors  {sslPolicyErrors} </li>" +
+                $"<li>{chainStatusTable}</li></ol></p>");
+
             if (sslPolicyErrors == SslPolicyErrors.None)
             {
                 return true;
             }
             else
             {
-                WriteSection("SSL Policy Errors", $"<p class='text-red-500'>{sslPolicyErrors}</p>");
+
                 return false;
             }
         }
@@ -151,7 +181,7 @@ namespace HttpsTrace
             if (acceptableIssuers != null && acceptableIssuers.Length > 0)
             {
                 string issuerList = string.Join("", Array.ConvertAll(acceptableIssuers, issuer => $"<li>{issuer}</li>"));
-                WriteSection("Acceptable Issuers", $"<ul class='list-disc ml-6'>{issuerList}</ul>");
+                WriteSection("Acceptable Issuers for the client certificate", $"<ul class='list-disc ml-6'>{issuerList}</ul>");
             }
             if (!string.IsNullOrEmpty(Options.CertificatePath))
             {
@@ -171,7 +201,7 @@ namespace HttpsTrace
             }
             else
             {
-               // WriteSection("Error", "<p class='text-red-500'>Certificate path is empty or null.</p>");
+                // WriteSection("Error", "<p class='text-red-500'>Certificate path is empty or null.</p>");
                 return null;
             }
         }
